f8431c9e55658bea5ff53b0e6f56fdeb90f35dd2
Duke Leonard Chazewik VI of Ohio <116516051+Chadman300@users.noreply.github.com>
2023-02-17 17:17:20 -0800

    Add files via upload

---
+++ VBPL/VBPL/shell.py
@@ -0,0 +1,18 @@
+import basic
+
+while True:
+    #input
+    text = input('VBPL > ') 
+    if text.strip() == "": continue
+    if text ==  "exit":
+        break
+
+    result, error = basic.run('<script.vbpl>', text)
+
+    if error:
+        print(error.as_string())
+    #elif result: 
+    #    if len(result.elements) == 1:
+    #        print(repr(result.elements[0]))
+    #    else:
+    #        print(repr(result))
